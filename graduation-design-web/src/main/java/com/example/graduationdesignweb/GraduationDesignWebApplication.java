aa9a1089093c105d09825ef60a5e9d5381f0945d
刘楠 <1095576371@qq.com>
2020-06-25 22:48:35 +0800

    graduation design web初始化

---
+++ graduation-design-web/src/main/java/com/example/graduationdesignweb/GraduationDesignWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.graduationdesignweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationDesignWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( GraduationDesignWebApplication.class, args );
+    }
+
+}
